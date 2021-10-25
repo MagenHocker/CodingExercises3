65e50b649c75bb97e7db6496347122beb436aa2e
Magen Hocker <70960438+MagenHocker@users.noreply.github.com>
2021-10-24 22:23:29 -0400

    Set up for Main file

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    /*
+    OOP Part 1 & 2: Classes, Constructors, Inheritance, Encapsulation, Polymorphism
+    - A project containing the exercises to the Java Programming Masterclass Java 11 & 17
+    - Exercises for Section 6 and 7
+    - The main file will contain test cases (not junit tests :( but print to console)
+    - tbh this won't be good testing at all I won't do all base cases etc just enough to debug
+     */
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
